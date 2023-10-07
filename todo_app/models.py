9f14a110e4186b639d65f72579bf56824e6dcd84
Bhagya Unnikrishnan <bhagyanandhu2002@gmail.com>
2023-10-08 00:04:54 +0530

    updated login and register

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Todo(models.Model):
+    id =  models.AutoField(primary_key=True)
+    task = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    
 # Create your models here.
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,15 +1,59 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib import messages
 
 def index(request):
-    return render(request, "index.html")
+    name = request.user.username
+    return render(request, "index.html", {'name':name})
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.create_user(username, password=password)
+            user.email = email
+        
+            user.save()
+        except IntegrityError:
+            return render(request, "register.html", {
+                "messages": ["Username already taken."]
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+        
+
     return render(request, "register.html")
 
-def login(request):
+def login_view(request):
+    if request.method != "POST":
+        return (
+            HttpResponseRedirect(reverse("index"))
+            if request.user.is_authenticated
+            else render(request, "login.html")
+        )
+    # Attempt to sign user in
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+
+    # Check if authentication successful
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        messages.error(request, "Invalid username and/or password.")
+        return redirect(reverse(login_view))
     return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
 
 # Create your views here.
