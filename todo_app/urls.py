13574089f76dd505af8f8e0dc2ff64b44d8e3d1a
Bhagya Unnikrishnan <bhagyanandhu2002@gmail.com>
2023-10-09 13:02:28 +0530

    updated

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path('delete/<int:identifier>/', views.delete_todo, name='delete'),
+    path('update/<int:identifier>/', views.update_todo, name='update'),
+    path('complete/<int:identifier>/', views.toggle_complete_todo, name='complete'),
 
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,13 +1,17 @@
 import datetime
+import json
 from django.shortcuts import render, redirect
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 from .models import Todo
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     name = request.user.username
     if request.method == 'POST':
@@ -62,10 +66,48 @@ def login_view(request):
     else:
         messages.error(request, "Invalid username and/or password.")
         return redirect(reverse(login_view))
-    return render(request, "login.html")
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("login"))
 
 
+@csrf_exempt
+def delete_todo(request, identifier):
+    if request.method == 'POST':
+        try:
+            todo = Todo.objects.get(id=identifier, username=request.user.username)
+            todo.delete()
+            return JsonResponse({'status': 200, 'msg': "Todo deleted"}, status=200)
+        except Todo.DoesNotExist:
+            return JsonResponse({'status': 404, 'msg': "The todo doesn't seem to exist"}, status=404)
+    else:
+        redirect(reverse(index))
+        
+@csrf_exempt
+def update_todo(request, identifier):
+    if request.method == 'POST':
+        try:
+            body = json.loads(request.body)
+            todo = Todo.objects.get(id=identifier, username=request.user.username)
+            todo.task = body['updatedTask']
+            todo.created_at = body['updatedDate']
+            todo.save()
+            return JsonResponse({'status': 200, 'msg': "Todo updated"}, status=200)
+        except Todo.DoesNotExist:
+            return JsonResponse({'status': 404, 'msg': "The todo doesn't seem to exist"}, status=404)
+    else:
+        redirect(reverse(index))
+
+@csrf_exempt
+def toggle_complete_todo(request, identifier):
+    if request.method == 'POST':
+        try:
+            todo = Todo.objects.get(id=identifier, username=request.user.username)
+            todo.completed = not todo.completed
+            todo.save()
+            return JsonResponse({'status': 200, 'msg': "Todo completed"}, status=200)
+        except Todo.DoesNotExist:
+            return JsonResponse({'status': 404, 'msg': "The todo doesn't seem to exist"}, status=404)
+    else:
+        redirect(reverse(index))
